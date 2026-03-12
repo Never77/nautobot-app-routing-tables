f9ee97f7098bb212f18e8db9097ac1d055eed46c
Hector GODZIEN <hgodzien@gmail.com>
2026-03-12 20:11:53 +0100

    Fixed configuration of the app
    Renamed the app with standard -app suffix for nautobot apps, fixed configuration location for the app and moved it into __init__ file, changed wrong informations for owner of the app.

--- nautobot_routing_tables/__init__.py
+++ nautobot_routing_tables/__init__.py
@@ -1,1 +1,19 @@
-__version__ = '1.1.0'
+from nautobot.apps import NautobotAppConfig
+
+__version__ = '1.1.1'
+
+class NautobotRoutingTablesConfig(NautobotAppConfig):
+    name = "nautobot_routing_tables"
+    verbose_name = "Nautobot Routing Tables"
+    description = "Model routing tables per device and VRF, including optional auto-managed connected routes."
+    version = "1.1.1"
+    author = "Never77"
+    base_url = "routing-tables"
+    min_version = "2.4.0"
+    max_version = "4.0.0"
+
+    config_view_name = "plugins:routing_tables:config"
+    menu_items = "routing_tables.navigation.menu_items"
+
+
+config = NautobotRoutingTablesConfig
